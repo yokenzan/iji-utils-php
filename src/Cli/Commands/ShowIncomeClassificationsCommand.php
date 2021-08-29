451e5953d671dc3f026561fb87a04636b4010918
yokenzan <31175068+yokenzan@users.noreply.github.com>
2021-08-30 03:08:14 +0900

    implement a command listing income classifications

--- src/Cli/Commands/ShowIncomeClassificationsCommand.php
+++ src/Cli/Commands/ShowIncomeClassificationsCommand.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace IjiUtils\Cli\Commands;
 
+use IjiUtils\MedicalFee\Amount\Burden\KogakuRyoyohi\ElderlyIncomeClassification;
 use IjiUtils\MedicalFee\Amount\Burden\KogakuRyoyohi\NonElderlyIncomeClassification;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -39,11 +41,35 @@ class ShowIncomeClassificationsCommand extends Command
     /**
      * {@inheritDoc}
      */
-    protected function execute(InputInterface $_input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $nonElderlyClassifications = NonElderlyIncomeClassification::values();
+        $tableHeaderRow = ['所得区分キー', '表示名称'];
 
-        $output->writeln(implode("\n", $nonElderlyClassifications));
+        if (!$input->getOption('exclude-non-elderly')) {
+            $output->writeln('70歳未満：');
+            $table = new Table($output);
+
+            foreach (NonElderlyIncomeClassification::values() as $classification) {
+                list('value' => $key, 'name' => $name) = $classification->jsonSerialize();
+                $table->setHeaders($tableHeaderRow);
+                $table->addRow([$key, $name]);
+            }
+
+            $table->render();
+        }
+
+        if (!$input->getOption('exclude-elderly')) {
+            $output->writeln('70歳以上：');
+            $table = new Table($output);
+
+            foreach (ElderlyIncomeClassification::values() as $classification) {
+                list('value' => $key, 'name' => $name) = $classification->jsonSerialize();
+                $table->setHeaders($tableHeaderRow);
+                $table->addRow([$key, $name]);
+            }
+
+            $table->render();
+        }
 
         return Command::SUCCESS;
     }
